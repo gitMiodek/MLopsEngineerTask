c603791499a5c2d6ba8c6027fc256ee75c2f1c3e
Jakub <kubamioduszewski@gmail.com>
2023-03-30 23:45:30 +0200

    feat(tests): Integration tests for an api

---
+++ API/Api_tests/test_api.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+URL = "http://0.0.0.0:8000/predict"
+
+
+# test if model recognize a plane
+def test_plane():
+    plane_img = "https://upload.wikimedia.org/wikipedia/commons/f/fc/Tarom.b737-700.yr-bgg.arp.jpg"
+    response = requests.post(url=URL, data=json.dumps({"image_url": plane_img}))
+
+    assert response.status_code == 200
+    assert response.json()['predicted_label'] == "Plane"
+
+
+# test if model recognize a car
+def test_car():
+    car_img = "https://upload.wikimedia.org/wikipedia/commons/2/25/2015_Mazda_MX-5_ND_2.0_SKYACTIV-G_160_i-ELOOP_Rubinrot-Metallic_Vorderansicht.jpg"
+    response = requests.post(url=URL, data=json.dumps({"image_url": car_img}))
+    assert response.status_code == 200
+    assert response.json()['predicted_label'] == "Car"
