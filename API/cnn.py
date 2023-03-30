c7be78b434f3f407d6f3f858d8ed8ee321a33b6f
Jakub <kubamioduszewski@gmail.com>
2023-03-30 23:43:12 +0200

    feat(model_instance): loading model instance with saved state dict

---
+++ API/cnn.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import torch
+from torchvision.models.resnet import ResNet18_Weights
+from torchvision import models
+
+#inicialize model
+model = models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 2)
+# Load trained state dict
+state_dict = torch.load('plane_car_state_dict.pt')
+model.load_state_dict(state_dict)
+# Turn model into inference mode
