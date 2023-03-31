364c88fa9ef0032413692a90771f7287e010b285
Jakub <kubamioduszewski@gmail.com>
2023-03-31 02:34:58 +0200

    hihih

--- API/Api_tests/test_api.py
+++ API/Api_tests/test_api.py
@@ -1,13 +1,14 @@
 import requests
 import json
 
-URL = "http://0.0.0.0:8000/predict"
+URL1 = "http://localhost:8000/predict"
+URL2 = "http://172.17.0.1:8000/predict"
 
 
 # test if model recognize a plane
 def test_plane():
     plane_img = "https://upload.wikimedia.org/wikipedia/commons/f/fc/Tarom.b737-700.yr-bgg.arp.jpg"
-    response = requests.post(url=URL, data=json.dumps({"image_url": plane_img}))
+    response = requests.post(url=URL1, data=json.dumps({"image_url": plane_img}))
 
     assert response.status_code == 200
     assert response.json()['predicted_label'] == "Plane"
@@ -16,6 +17,6 @@ def test_plane():
 # test if model recognize a car
 def test_car():
     car_img = "https://upload.wikimedia.org/wikipedia/commons/2/25/2015_Mazda_MX-5_ND_2.0_SKYACTIV-G_160_i-ELOOP_Rubinrot-Metallic_Vorderansicht.jpg"
-    response = requests.post(url=URL, data=json.dumps({"image_url": car_img}))
+    response = requests.post(url=URL2, data=json.dumps({"image_url": car_img}))
     assert response.status_code == 200
     assert response.json()['predicted_label'] == "Car"
--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,6 @@ WORKDIR /API
 EXPOSE 8000
 # Run application
 
-CMD ["uvicorn","app:app","--host","0.0.0.0","--port","8000"]
+CMD ["uvicorn","app:app","--host","localhost","--port","8000"]
 
 
